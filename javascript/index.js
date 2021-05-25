0553f71477abb2ca9302797764e3abcde8db5096
fredyflemus <fredyflemus@gmail.com>
2021-05-24 23:50:25 -0600

    minor fixes

--- javascript/index.js
+++ javascript/index.js
@@ -2,29 +2,30 @@ closeUrl = "./icons/close.svg";
 openUrl = "./icons/list.svg";
 
 menuButton = document.getElementById("menu-button");
-menuButton.addEventListener("click", mostrarMenu);
+menuButton.addEventListener("touchstart", mostrarMenu);
 menu = document.getElementById("menu");
 darkWindow = document.getElementById("darkWindow");
 imagenMenu = document.getElementById("img-menu");
 
 
 
-function mostrarMenu(){
-    
+function mostrarMenu(evento){
+    evento.preventDefault();
     imagenMenu.src = closeUrl;
-    imagenMenu.width = "16";
+    
     menu.classList.add("show");
     darkWindow.classList.add("shadow");
-    darkWindow.addEventListener("click", cerrarMenu);
-    menuButton.addEventListener("click", cerrarMenu);
+    darkWindow.addEventListener("touchstart", cerrarMenu);
+    menuButton.addEventListener("touchstart", cerrarMenu);
 }
 
-function cerrarMenu(){
+function cerrarMenu(evento){
+    evento.preventDefault();
     menu.classList.remove("show");
     darkWindow.classList.remove("shadow");
-    darkWindow.removeEventListener("click", cerrarMenu);
-    menuButton.removeEventListener("click", cerrarMenu);
+    darkWindow.removeEventListener("touchstart", cerrarMenu);
+    menuButton.removeEventListener("touchstart", cerrarMenu);
     imagenMenu.src = openUrl;
-    imagenMenu.width = "20";
+    
     
 }

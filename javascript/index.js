4ea18183d04b029529ab99ec961d3b7c516463ed
fredyflemus <fredyflemus@gmail.com>
2021-06-08 00:36:48 -0600

    js functions

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,47 @@
-closeUrl = "./icons/close.svg";
-openUrl = "./icons/list.svg";
+let closeUrl = "./icons/close.svg";
+let openUrl = "./icons/list.svg";
 
-menuButton = document.getElementById("menu-button");
+
+let menuButton = document.getElementById("menu-button");
 menuButton.addEventListener("touchstart", mostrarMenu);
-menu = document.getElementById("menu");
-darkWindow = document.getElementById("darkWindow");
-imagenMenu = document.getElementById("img-menu");
+let menu = document.getElementById("menu");
+let darkWindow = document.getElementById("darkWindow");
+let imagenMenu = document.getElementById("img-menu");
+
+let headerMenu = document.getElementById("header-menu");
+
+headerMenu.addEventListener("click", clickEnMenu);
+menu.addEventListener("touchstart", touchEnMenu);
 
 
+function touchEnMenu(){
+    console.log("hola mamu");
+}
+
+function clickEnMenu(evento){
+    console.log(evento);
+    if(evento.target.nodeName === "A"){
+        let seleccion = evento.target;
+        seleccion.classList.add("is-active");
+        let hermanoLeft = seleccion.parentNode.previousElementSibling;
+        let hermanoRigth = seleccion.parentNode.nextElementSibling;
+        
+        while(hermanoLeft != undefined){
+            hermanoLeft = hermanoLeft.firstChild;
+            hermanoLeft.classList.remove("is-active");
+            hermanoLeft = hermanoLeft.parentNode.previousElementSibling;
+        }
+
+        while(hermanoRigth != undefined){
+            hermanoRigth = hermanoRigth.firstChild;
+            hermanoRigth.classList.remove("is-active");
+            hermanoRigth = hermanoRigth.parentNode.nextElementSibling;
+        }
+        
+        
+
+    }
+}
 
 function mostrarMenu(evento){
     evento.preventDefault();
